6628cfc92bc6d78e1dd64eb06d75b0992dd37fbb
manutd2008thebest <manutd2008thebest@gmail.com>
2025-03-31 13:46:47 +0500

    feat: add backend url

--- context/userContext.js
+++ context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "https://taskfyer.onrender.com";
+  const serverUrl = "https://backend-au7l.onrender.com";
 
   const router = useRouter();
 
